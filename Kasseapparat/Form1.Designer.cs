51f5ba35c0be64cfea5a62ba2bec7f8cd3840418
Jens Jensen <jej@eucnord.dk>
2020-01-30 15:40:46 +0100

    Add project files.

---
+++ Kasseapparat/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Kasseapparat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbPris = new System.Windows.Forms.TextBox();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.lblSubtotal = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSum = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblDagsTotal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbPris
+            // 
+            this.tbPris.Location = new System.Drawing.Point(161, 71);
+            this.tbPris.Name = "tbPris";
+            this.tbPris.Size = new System.Drawing.Size(100, 26);
+            this.tbPris.TabIndex = 0;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(301, 71);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(55, 26);
+            this.btnEnter.TabIndex = 1;
+            this.btnEnter.Text = "E";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // lblSubtotal
+            // 
+            this.lblSubtotal.AutoSize = true;
+            this.lblSubtotal.Location = new System.Drawing.Point(157, 106);
+            this.lblSubtotal.Name = "lblSubtotal";
+            this.lblSubtotal.Size = new System.Drawing.Size(51, 20);
+            this.lblSubtotal.TabIndex = 2;
+            this.lblSubtotal.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Subtotal";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Pris";
+            // 
+            // btnSum
+            // 
+            this.btnSum.Location = new System.Drawing.Point(301, 102);
+            this.btnSum.Name = "btnSum";
+            this.btnSum.Size = new System.Drawing.Size(75, 30);
+            this.btnSum.TabIndex = 5;
+            this.btnSum.Text = "Sum";
+            this.btnSum.UseVisualStyleBackColor = true;
+            this.btnSum.Click += new System.EventHandler(this.btnSum_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Total";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(59, 196);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Dags Total";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(157, 154);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(51, 20);
+            this.lblTotal.TabIndex = 8;
+            this.lblTotal.Text = "label5";
+            // 
+            // lblDagsTotal
+            // 
+            this.lblDagsTotal.AutoSize = true;
+            this.lblDagsTotal.Location = new System.Drawing.Point(157, 196);
+            this.lblDagsTotal.Name = "lblDagsTotal";
+            this.lblDagsTotal.Size = new System.Drawing.Size(51, 20);
+            this.lblDagsTotal.TabIndex = 9;
+            this.lblDagsTotal.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblDagsTotal);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblSubtotal);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.tbPris);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbPris;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Label lblSubtotal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSum;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblDagsTotal;
+    }
+}
+
---
+++ Kasseapparat/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kasseapparat
+{
+    public partial class Form1 : Form
+    {
+        decimal subtotal;
+        decimal total;
+        decimal dagstotal;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Opdater();
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            subtotal = subtotal + Decimal.Parse(tbPris.Text);
+            lblSubtotal.Text = subtotal.ToString();
+            
+            Opdater();
+        }
+
+        private void btnSum_Click(object sender, EventArgs e)
+        {
+            total = subtotal;
+            dagstotal = dagstotal + total;
+            subtotal = 0;
+
+            Opdater();         
+        }
+        
+        private void Opdater()
+        {
+            lblTotal.Text = total.ToString();
+            lblDagsTotal.Text = dagstotal.ToString();
+            lblSubtotal.Text = subtotal.ToString();
+            tbPris.Text = "";
+        }
+    }
+}
